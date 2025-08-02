9172527a8cf9f46ecf7626439441e47bde2b005a
ashaldenkov <a.shaldenkov@yandex.ru>
2025-08-02 18:29:34 +0900

    admin: updated default page so after login user is redirected there instead of 404

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -40,7 +40,8 @@ const Login = ({setToken}) => {
             </form>
         </div>
         <div className='flex flex-col items-center mt-1 bg-gray-100 shadow-md px-4 py-2 rounded-2xl z-10 text-gray-700 '>
-            <p>Tip: email: admin@test.com</p>
+            <p>Tip</p>
+            <p>email: admin@test.com</p>
             <p>password: 123qwe123</p>
         </div>
     </div>
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
   return (
     <div className='w-[18%] min-h-screen border-r-2'>
         <div className='flex flex-col gap-4 pt-6 lp-[20%] text-base '>
-            <NavLink to='/add' className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l'>
+            <NavLink to='/' className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l'>
                 <img className='w-5 h-5' src={assets.addIcon} alt='add-icon' />
                 <p className='hidden md:block'>Add items</p>
             </NavLink>
