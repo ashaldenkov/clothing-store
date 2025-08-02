7f6c9ef6ca52c31ad2417be636a4cf24138ace1e
ashaldenkov <a.shaldenkov@yandex.ru>
2025-08-02 22:36:36 +0900

    front: addded fetchpriority to image

--- frontend/src/features/shared/ProductItem.jsx
+++ frontend/src/features/shared/ProductItem.jsx
@@ -9,7 +9,7 @@ const ProductItem = ({id, image, name, price}) => {
   return (
     <Link to={`/product/${id}`} className='text-gray-700 cursor-pointer group' >
         <div className='overflow-hidden rounded-xl'>
-            <img src={image[0]} className='w-full aspect-square transistion-all duration-300 ease-in-out group-hover:scale-[115%] aspect-square object-cover' alt=''/>
+            <img src={image[0]} fetchpriority='high' className='w-full aspect-square transistion-all duration-300 ease-in-out group-hover:scale-[115%] aspect-square object-cover' alt=''/>
         </div> 
         <p className='pt-3 pb-1 text-sm'>{name}</p>
         <p className='text-sm font-medium'>{currency}{price}</p>
